7f308024601d8964d45978d144bf88683fc54d23
Saomya Badoniya <91871962+saomyaa@users.noreply.github.com>
2023-07-07 22:47:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+#import the model
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
+
+
+
+st.title("Laptop Price Predictor")
+#brand
+company = st.selectbox('Brand',df['Company'].unique())
+#type of laptop
+type = st.selectbox('Type',df['TypeName'].unique())
+#Ram
+ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+#weight
+weight = st.number_input('Weight of the laptop')
+#touchscreen
+touchscreen = st.selectbox('TouchScreen',['No','Yes'])
+#ips
+ips= st.selectbox('IPS',['No','Yes'])
+#ppi
+screen_size = st.number_input('Screen Size')
+#resolution
+resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+#cpu
+cpu = st.selectbox('CPU',df['Cpu Brand'].unique())
+#hdd
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+#ssd
+ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+#gpu
+gpu = st.selectbox('GPU',df['Gpu_Brand'].unique())
+#os
+os = st.selectbox('OS',df['OS'].unique())
+
+if st.button('Predicted Price'):
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen=1
+    else:
+        touchscreen=0
+    if ips == 'Yes':
+        ips=1
+    else:
+        ips=0
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res**2 + Y_res**2))**0.5/screen_size
+    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+    query = query.reshape(1,12)
+    st.title(np.exp(pipe.predict(query)))
+
+
